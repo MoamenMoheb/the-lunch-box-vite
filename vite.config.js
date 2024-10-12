d1096877f6603ad8088bba4b9576220aaa2875e3
Moamen.Moheb <moamenmogeb9@gmail.com>
2024-10-12 20:31:06 +0300

    add base

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/the-lunch-box-vite/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
